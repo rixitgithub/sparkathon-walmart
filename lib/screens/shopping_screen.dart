30438ceaedaa029f80ea3edc5aeeffd3f267eea7
Rishit Tiwari <irishittiwari@gmail.com>
2024-07-31 11:39:26 +0530

    camera access

--- lib/screens/shopping_screen.dart
+++ lib/screens/shopping_screen.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ShoppingScreen extends StatelessWidget {
+class ShoppingScreen extends StatefulWidget {
+  @override
+  _ShoppingScreenState createState() => _ShoppingScreenState();
+}
+
+class _ShoppingScreenState extends State<ShoppingScreen> {
+  File? _image;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _captureImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+      // TODO: Pass the image to your AI model for processing
+    } else {
+      print('No image selected.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,15 +35,13 @@ class ShoppingScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Capture the cart image to detect items.'),
+            if (_image != null)
+              Image.file(_image!)
+            else
+              Text('Capture the cart image to detect items.'),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // TODO: Handle image capture and item detection using AI model
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Capture Image functionality to be implemented')),
-                );
-              },
+              onPressed: _captureImage,
               child: Text('Capture Image'),
             ),
           ],
